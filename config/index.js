1f45af3937c84eb9523bab92bd018eaad56f98e1
unknown <1543859741@qq.com>
2020-12-17 21:21:20 +0800

    项目整合

--- config/index.js
+++ config/index.js
@@ -23,11 +23,7 @@ module.exports = {
     // Use Eslint Loader?
     // If true, your code will be linted during bundling and
     // linting errors and warnings will be shown in the console.
-<<<<<<< HEAD
     // useEslint: flase,
-=======
-    // useEslint: true,
->>>>>>> d377d02 (整合项目)
     // If true, eslint errors and warnings will also be shown in the error overlay
     // in the browser.
     showEslintErrorsInOverlay: false,
